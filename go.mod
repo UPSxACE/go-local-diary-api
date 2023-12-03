4f30695e55504c89a6194d72a1ccff328afc2e4f
Eduardo Botelho <eduardo11224b@gmail.com>
2023-12-03 14:31:10 +0000

    first tests with boltdb

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/UPSxACE/go-local-diary-api
 go 1.21.3
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible
+	github.com/boltdb/bolt v1.3.1
 	github.com/labstack/echo/v4 v4.11.3
 )
 
--- server.go
+++ server.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
+	"time"
 
 	"github.com/UPSxACE/go-local-diary-api/controllers"
 	"github.com/UPSxACE/go-local-diary-api/views"
+	"github.com/boltdb/bolt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		fmt.Println("ServerConfig: ", tx.Bucket([]byte("ServerConfig")))
+		serverConfigBucket, err := tx.CreateBucketIfNotExists([]byte("ServerConfig"))
+		if(err != nil){
+			return err
+		}
+
+		initialized := serverConfigBucket.Get([]byte("initialized"))
+		if(initialized == nil){
+			fmt.Println("First time initializing the server")
+			serverConfigBucket.Put([]byte("initialized"), []byte("true"))
+			fmt.Println("Now initialized!")
+		}
+
+		initialized = serverConfigBucket.Get([]byte("initialized"))
+		fmt.Printf("Server initialized: %s\n", initialized)
+
+		//if err := initBucket.Put(true)
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Pre-compile templates in views subdirectories, and subdirectories of those subdirectories
 	tBuilder :=  template.Must(template.ParseGlob("views/*/*.html"));
 	tBuilder = template.Must(tBuilder.ParseGlob("views/*/*/*.html"))
