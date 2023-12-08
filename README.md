d20a374f1913523e70b2e7de2249e8c79689d9bc
Eduardo Botelho <eduardo11224b@gmail.com>
2023-12-08 19:08:37 +0000

    now my dev-script shows output too

--- README.md
+++ README.md
@@ -63,7 +63,7 @@
 ## Alternative for running in development mode
 <ul>
   <li>
-    In case you don't need any chat inputs, you can run all commands above at once using:
+    In case you don't need to read any command line output, you can run all commands above at once using:
     <ul>
       <li>go run ./dev-script</li>
     </ul>
--- dev-script/main.go
+++ dev-script/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"os"
 	"os/exec"
 	"sync"
 )
 
 func execCommand(command [2]string, waitGroup sync.WaitGroup) {
-	cmd := exec.Command("cd" , "../")
+	cmd := exec.Command("cd", "../")
 	cmd = exec.Command(command[0], command[1])
 
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println("Error on command:", command[0], command[1])
-		log.Fatal(err)
+	var stdBuffer bytes.Buffer
+
+	mw := io.MultiWriter(os.Stdout, &stdBuffer)
+	cmd.Stdout = mw
+	cmd.Stderr = mw
+
+	// Execute the command
+	if err := cmd.Run(); err != nil {
+		log.Panic(err)
 	}
-	
+
 	waitGroup.Done()
 }
 
@@ -24,11 +32,11 @@ func main() {
 	fmt.Println("Executing all 6 dev commands...")
 	var wg sync.WaitGroup
 	wg.Add(6)
-	go execCommand([2]string{"make", "tailwind-watch"},wg)
-	go execCommand([2]string{"make", "storybook"},wg)
-	go execCommand([2]string{"make", "webpack-watch"},wg)
-	go execCommand([2]string{"air", ""},wg)
-	go execCommand([2]string{"make", "docs"},wg)
-	go execCommand([2]string{"make", "swag"},wg)
+	go execCommand([2]string{"make", "tailwind-watch"}, wg)
+	go execCommand([2]string{"make", "storybook"}, wg)
+	go execCommand([2]string{"make", "webpack-watch"}, wg)
+	go execCommand([2]string{"air", ""}, wg)
+	go execCommand([2]string{"make", "docs"}, wg)
+	go execCommand([2]string{"make", "swag"}, wg)
 	wg.Wait()
 }
